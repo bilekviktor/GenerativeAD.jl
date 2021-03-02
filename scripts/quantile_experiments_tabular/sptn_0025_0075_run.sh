33f9ec723534b165882e2f213a0333f7fea93d89
bilekviktor <viktor.bilek97@gmail.com>
2021-03-02 08:26:03 +0100

    modify run scripts

--- scripts/quantile_experiments_tabular/sptn_0025_0075_run.sh
+++ scripts/quantile_experiments_tabular/sptn_0025_0075_run.sh
@@ -13,4 +13,4 @@ module load Python/3.8.2-GCCcore-9.3.0
 
 julia --project -e 'using Pkg; Pkg.instantiate();'
 
-julia --project ./sptn.jl $MAX_SEED $DATASET $CONTAMINATION
+julia --project ./sptn_0025_0075.jl $MAX_SEED $DATASET $CONTAMINATION
--- scripts/quantile_experiments_tabular/sptn_005_010_run.sh
+++ scripts/quantile_experiments_tabular/sptn_005_010_run.sh
@@ -13,4 +13,4 @@ module load Python/3.8.2-GCCcore-9.3.0
 
 julia --project -e 'using Pkg; Pkg.instantiate();'
 
-julia --project ./sptn.jl $MAX_SEED $DATASET $CONTAMINATION
+julia --project ./sptn_005_010.jl $MAX_SEED $DATASET $CONTAMINATION
