7ba1c3fd160c1002ba8f7deed7b58139daa279f2
bilekviktor <viktor.bilek97@gmail.com>
2021-03-23 16:38:13 +0100

    add relative nmb of parameters to rank info (script)

---
+++ scripts/evaluation/evaluate_npar_rank.jl
@@ -0,0 +1,114 @@
+using ArgParse
+using DrWatson
+@quickactivate
+using CSV
+using BSON
+using Random
+using FileIO
+using DataFrames
+using PrettyTables
+using PrettyTables.Crayons
+
+using GenerativeAD.Evaluation: _prefix_symbol, PAT_METRICS, aggregate_stats_mean_max
+using GenerativeAD.Evaluation: rank_table, print_rank_table
+using GenerativeAD.Datasets: load_data
+
+s = ArgParseSettings()
+@add_arg_table! s begin
+	"filename"
+		arg_type = String
+		default = "evaluation/images_eval.bson"
+		help = "Location of cached DataFrame."
+	"-c", "--criterion-metric"
+		arg_type = String
+		default = "val_auc"
+		help = "Criterion to sort the models."
+	"-r", "--rank-metric"
+		arg_type = String
+		default = ""
+		help = "Metric to rank models."
+	"-o", "--output-prefix"
+		arg_type = String
+		default = "evaluation/images_eval"
+		help = "Output prefix for storing results."
+    "-f", "--force"
+    	action = :store_true
+		help = "Overwrite all generated files."
+end
+
+#=
+data_dim(s) = size(load_data(s)[1][1], 1)
+datadim = Dict()
+for d in unique(df[:dataset])
+    println(d)
+    datadim[d] = size(load_data(d)[1][1], 1)
+end
+df[data_dim.(df[:dataset]) .== 10]
+
+cdf.datadim = 1
+cdf.relnpars = 1.0
+for f in eachrow(cdf)
+    @show f.datadim = datadim[f[:dataset]]
+    @show f.relnpars = f[:npars]/f[:datadim]
+end
+
+df_agg = aggregate_stats_mean_max(cdf, "val_auc")
+rt = rank_table(df_agg, "tst_auc")
+
+npar_df = DataFrame(modelname = String[],relnpars = Float64[], rank = Float64[])
+for i in minimum(cdf[:relnpars]):10:maximum(cdf[:relnpars])
+    df_agg = aggregate_stats_mean_max(cdf[cdf[:relnpars] .<= i, :], "val_auc")
+    rt = rank_table(df_agg, "tst_auc")
+    for n in names(rt)[2:end]
+        push!(npar_df, (n, i, rt[end, n]))
+    end
+    println(i)
+end
+npar_df
+using Plots
+sptn_df = npar_df[npar_df[:modelname] .== "sptn", :]
+sptn_5_df = npar_df[npar_df[:modelname] .== "sptn_005_010", :]
+sptn_25_df = npar_df[npar_df[:modelname] .== "sptn_0025_0075", :]
+plot(sptn_df[:relnpars], sptn_df[:rank])
+plot!(sptn_5_df[:relnpars], sptn_5_df[:rank])
+plot!(sptn_25_df[:relnpars], sptn_25_df[:rank])
+=#
+
+function main(args)
+	f = datadir(args["filename"])
+	df = load(f)[:df]
+    cdf = deepcopy(df)
+    cdf.datadim = 1
+    cdf.relnpars = 1.0
+    for f in eachrow(cdf)
+        f.datadim = datadim[f[:dataset]]
+        #relnpars = numb. of params of model / data dim.
+        f.relnpars = f[:npars]/f[:datadim]
+    end
+	@info "Loaded $(nrow(df)) rows from $f"
+
+    # Dataframe for final ranks for each relnpars
+    npar_df = DataFrame(modelname = String[],relnpars = Float64[], rank = Float64[])
+
+    for i in minimum(cdf[:relnpars]):10:maximum(cdf[:relnpars])
+        # evaluation and rank table
+        df_agg = aggregate_stats_mean_max(cdf[cdf[:relnpars] .<= i, :], Symbol(args["criterion-metric"]))
+        rt = rank_table(df_agg, args["rank-metric"])
+        for n in names(rt)[2:end]
+            # row for each model at relnpars
+            push!(npar_df, (n, i, rt[end, n]))
+        end
+    end
+
+	@info "Best models chosen by $(args["criterion-metric"])"
+	@info "Ranking by $(args["rank-metric"])"
+
+    ff = datadir(args["output-prefix"])
+	if (isfile(ff) && args["force"]) || ~isfile(ff)
+		@info "Saving $(nrow(df)) rows to $f."
+		wsave(ff, Dict(:df => df))
+	end
+	@info "---------------- DONE -----------------"
+end
+
+main(parse_args(ARGS, s))
---
+++ scripts/evaluation/utils/plot_npar_rank.jl
@@ -0,0 +1,21 @@
+using Plots
+using ArgParse
+using DrWatson
+@quickactivate
+using CSV
+using BSON
+using Random
+using FileIO
+using DataFrames
+
+function plot_npar_rank(s)
+    f = load(s)
+    df = f[:df]
+    pl = plot()
+    for n in unique(df[:modelname])
+        n_df = df[df[:modelname] .== n, :]
+        plot!(n_df[:relnpars], n_df[:rank], label = n)
+    end
+    display(pl)
+    return pl
+end
